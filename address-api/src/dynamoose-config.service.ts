ae55a533c775187c2c82bee47abc612c0e9f9f3c
Zamoca <suntail2002@naver.com>
2023-04-06 17:29:58 +0900

    refactor: 환경변수 추가

--- address-api/src/dynamoose-config.service.ts
+++ address-api/src/dynamoose-config.service.ts
@@ -7,9 +7,9 @@ export class DynamooseConfigService implements DynamooseOptionsFactory {
   createDynamooseOptions(): DynamooseModuleOptions {
     return {
       aws: {
-        region: 'local',
-        accessKeyId: 'fakeAccessKeyId',
-        secretAccessKey: 'fakeSecretAccessKey',
+        region: process.env.AWS_DEFAULT_REGION,
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
       },
       local: 'http://localhost:8000',
     };
