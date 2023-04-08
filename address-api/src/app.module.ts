979b63d111377c084fe29acbc45179886002b55e
Zamoca <suntail2002@naver.com>
2023-04-08 22:29:12 +0900

    remove: 불필요한 파일 삭제

--- address-api/src/app.module.ts
+++ address-api/src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ContactModule } from './contact/contact.module';
 import { DynamooseModule } from 'nestjs-dynamoose';
 
 @Module({
   imports: [DynamooseModule.forRoot(), ContactModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
