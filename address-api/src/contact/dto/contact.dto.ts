aa87620c8cbbc9c9f21541fca9d493d0982e789c
Zamoca <suntail2002@naver.com>
2023-04-08 22:25:50 +0900

    refactor: DTO와 Entity 나누기

--- address-api/src/contact/dto/contact.dto.ts
+++ address-api/src/contact/dto/contact.dto.ts
@@ -1,16 +1,13 @@
-import { IsEmail, IsNotEmpty, IsPhoneNumber, IsString } from 'class-validator';
+import { IsEmail, IsPhoneNumber, IsString } from 'class-validator';
 import { PartialType } from '@nestjs/mapped-types';
 
 export class ContactDto {
-  @IsString()
   @IsPhoneNumber()
   phoneNumber: string;
 
   @IsString()
-  @IsNotEmpty()
   name: string;
 
-  @IsString()
   @IsEmail()
   email: string;
 }
---
+++ address-api/src/contact/entities/contact.entity.ts
@@ -0,0 +1,10 @@
+export class contactKey {
+  id: number;
+}
+
+export class Contact extends contactKey {
+  id: number;
+  name: string;
+  phoneNumber: string;
+  email: string;
+}
